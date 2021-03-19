cca7477582f8c4000a5ed7e479bfab99ba5ed9fd
Shadab Shaikh <johndoe@example.com>
2021-03-19 16:21:51 -0400

    Update JavaGrep

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrep.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrep.java
@@ -6,15 +6,37 @@ import java.io.File;
 import java.util.List;
 
 public interface JavaGrep {
-
+  /**
+   * Top level search workflow
+   * throw IOException
+   * */
   void process() throws IOException;
 
+  /**
+   * Traversal a given directory and return all files
+   * @param rootDir input directory
+   * @return file under the rootDir
+   * */
   List<File> listFiles(String rootDir);
 
+  /**
+   * read a file and return all lines
+   * @param inputFile input file
+   * @return lines
+   * @throws IllegalArgumentException if inputFile is not a File
+   * */
   List<String> readLines(File inputFile) throws FileNotFoundException;
 
+  /**
+   * check if the line contains pattern
+   * @param line A string
+   * */
   boolean containsPattern(String line);
 
+  /**
+   * write liens to file
+   * @param lines array of line
+   * */
   void writeToFile(List<String> lines) throws IOException;
 
   String getRootPath();

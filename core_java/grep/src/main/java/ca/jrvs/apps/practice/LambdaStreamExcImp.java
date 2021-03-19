0946298f6b97254d4d40990f9f1a4fc67b71c595
Shadab Shaikh <johndoe@example.com>
2021-03-19 15:59:34 -0400

    update LambdaStreamExcImp

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/LambdaStreamExcImp.java
@@ -0,0 +1,84 @@
+package ca.jrvs.apps.practice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class LambdaStreamExcImp implements LambdaStreamExc {
+
+  @Override
+  public Stream<String> createStrStream(String... strings) {
+    return Arrays.stream(strings);
+
+  }
+
+  @Override
+  public Stream<String> toUpperCase(String... strings) {
+    return createStrStream(strings).map(String::toUpperCase);
+  }
+
+  @Override
+  public Stream<String> filter(Stream<String> stringStream, String pattern) {
+    String patternRegex = ".*" + pattern + ".*";
+    return stringStream.filter(str -> !str.matches(patternRegex));
+  }
+
+  @Override
+  public IntStream createIntStream(int[] arr) {
+    return Arrays.stream(arr);
+  }
+
+  @Override
+  public <E> List<E> toList(Stream<E> stream) {
+    return stream.collect(Collectors.toList());
+  }
+
+  @Override
+  public List<Integer> toList(IntStream intStream) {
+    return intStream.mapToObj(i -> i).collect(Collectors.toList());
+  }
+
+  @Override
+  public IntStream createIntStream(int start, int end) {
+    return IntStream.rangeClosed(start, end);
+  }
+
+  @Override
+  public DoubleStream squareRootIntStream(IntStream intStream) {
+    return intStream.mapToDouble(s -> {
+      return Math.sqrt(s);
+    });
+  }
+
+  @Override
+  public IntStream getOdd(IntStream intStream) {
+    return intStream.filter(num -> num % 2 == 1);
+  }
+
+  @Override
+  public Consumer<String> getLambdaPrinter(String prefix, String suffix) {
+    return (String message) -> {
+      System.out.println(prefix + message + suffix);
+    };
+  }
+
+  @Override
+  public void printMessages(String[] messages, Consumer<String> printer) {
+    Arrays.stream(messages).forEach(s -> printer.accept(s));
+  }
+
+  @Override
+  public void printOdd(IntStream intStream, Consumer<String> printer) {
+    getOdd(intStream).forEach(t -> {printer.accept(String.valueOf(t));});
+
+  }
+
+  @Override
+  public Stream<Integer> flatNestedInt(Stream<List<Integer>> ints) {
+    return ints.flatMap(numbers -> numbers.stream()).map(x -> x*x);
+  }
+}

911225587359b84e95ce1d136419289e7021d271
Shadab Shaikh <johndoe@example.com>
2021-03-26 10:57:53 -0400

    OrderDAO stash recovery

--- jdbc/src/main/java/ca/jrvs/apps/jdbc/Order.java
+++ jdbc/src/main/java/ca/jrvs/apps/jdbc/Order.java
@@ -1,2 +1,87 @@
-package ca.jrvs.apps.jdbc;public class Order {
+package ca.jrvs.apps.jdbc;
+
+import ca.jrvs.apps.jdbc.util.DataTransferObject;
+import java.util.Date;
+
+public class Order implements DataTransferObject{
+
+  private long id;
+  private Date creationDate;
+  private float total_due;
+  private String status;
+  private String salespersonFirstName;
+  private String salespersonLastName;
+  private String salespersonEmail;
+  private String Quantity;
+ /* private String code;
+  private String productName;
+  private String productSize;
+  private String productVariety;
+  private String productVariety; */
+
+
+  @Override
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public Date getCreationDate() {
+    return creationDate;
+  }
+
+  public void setCreationDate(Date creationDate) {
+    this.creationDate = creationDate;
+  }
+
+  public float getTotal_due() {
+    return total_due;
+  }
+
+  public void setTotal_due(float total_due) {
+    this.total_due = total_due;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public String getSalespersonFirstName() {
+    return salespersonFirstName;
+  }
+
+  public void setSalespersonFirstName(String salespersonFirstName) {
+    this.salespersonFirstName = salespersonFirstName;
+  }
+
+  public String getSalespersonLastName() {
+    return salespersonLastName;
+  }
+
+  public void setSalespersonLastName(String salespersonLastName) {
+    this.salespersonLastName = salespersonLastName;
+  }
+
+  public String getSalespersonEmail() {
+    return salespersonEmail;
+  }
+
+  public void setSalespersonEmail(String salespersonEmail) {
+    this.salespersonEmail = salespersonEmail;
+  }
+
+  public String getQuantity() {
+    return Quantity;
+  }
+
+  public void setQuantity(String quantity) {
+    Quantity = quantity;
+  }
 }
--- jdbc/src/main/java/ca/jrvs/apps/jdbc/OrderDAO.java
+++ jdbc/src/main/java/ca/jrvs/apps/jdbc/OrderDAO.java
@@ -1,2 +1,71 @@
-package ca.jrvs.apps.jdbc;public class OrderDAO {
+package ca.jrvs.apps.jdbc;
+
+import ca.jrvs.apps.jdbc.util.DataAccessObject;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class OrderDAO extends DataAccessObject<Order>{
+
+  private static final String GET_ONE = "SELECT\n"
+      + "  c.first_name, c.last_name, c.email, o.order_id,\n"
+      + "  o.creation_date, o.total_due, o.status,\n"
+      + "  s.first_name, s.last_name, s.email,\n"
+      + "  ol.quantity,\n"
+      + "  p.code, p.name, p.size, p.variety, p.price\n"
+      + "from orders o\n"
+      + "  join customer c on o.customer_id = c.customer_id\n"
+      + "  join salesperson s on o.salesperson_id=s.salesperson_id\n"
+      + "  join order_item ol on ol.order_id=o.order_id\n"
+      + "  join product p on ol.product_id = p.product_id\n"
+      + "where o.order_id = ?;";
+
+
+  public OrderDAO(Connection connection) {
+    super(connection);
+  }
+
+  @Override
+  public Order findById(long id) {
+    Order order = new Order();
+    try(PreparedStatement statement = this.connection.prepareStatement(GET_ONE);){
+      statement.setLong(1,id);
+      ResultSet rs = statement.executeQuery();
+      while (rs.next()) {
+        order.setId(rs.getLong("order_id"));
+        order.setCreationDate(rs.getDate("creation_date"));
+        order.setTotal_due(rs.getFloat("total_due"));
+        order.setStatus(rs.getString("status"));
+        order.setSalespersonFirstName(rs.getString("sales_first_name"));
+      }
+
+    }catch (SQLException e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
+
+    return null;
+  }
+
+  @Override
+  public List<Order> findAll() {
+    return null;
+  }
+
+  @Override
+  public Order update(Order dto) {
+    return null;
+  }
+
+  @Override
+  public Order create(Order dto) {
+    return null;
+  }
+
+  @Override
+  public void delete(long id) {
+
+  }
 }

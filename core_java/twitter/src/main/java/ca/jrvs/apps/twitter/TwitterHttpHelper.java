70d959bb969b598a975d39ab82a3cd6e811b0492
Shadab Shaikh <johndoe@example.com>
2021-04-01 13:03:38 -0400

    Twitter http helper

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/TwitterHttpHelper.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/TwitterHttpHelper.java
@@ -1,2 +1,21 @@
-package ca.jrvs.apps.twitter;public class TwitterHttpHelper {
+package ca.jrvs.apps.twitter;
+
+import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
+import java.net.URI;
+import sun.net.www.http.HttpClient;
+
+public class TwitterHttpHelper implements HttpHelper {
+
+  private OAuthConsumer consumer;
+  private HttpClient httpClient;
+
+  @Override
+  public HttpResponse httpPost(URI uri) {
+    return null;
+  }
+
+  @Override
+  public HttpResponse httpGet(URI uri) {
+    return null;
+  }
 }

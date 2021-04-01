aa9ef501e9a4c8fc8ebe621b02a3b9a377be3909
Shadab Shaikh <johndoe@example.com>
2021-04-01 13:01:55 -0400

    Twitter http helper

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/HttpHelper.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/HttpHelper.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class HttpHelper {
+package ca.jrvs.apps.twitter.dao.helper;
+
+import java.net.URI;
+import org.apache.http.HttpResponse;
+import org.apache.http.entity.StringEntity;
+
+public interface HttpHelper {
+
+  /**
+   * Execute a HTTP Post call
+   * @param uri
+   * @return
+   */
+  HttpResponse httpPost(URI uri);
+
+  /**
+   * Execute a HTTP Get call
+   * @param uri
+   * @return
+   */
+  HttpResponse httpGet(URI uri);
 }
